68b542480d596b7ee0d86824de5cc4f012e9c68a
Keichi Takahashi <keichi.t@me.com>
2016-06-05 23:30:27 -0400

    Add DB dump tool

---
+++ list.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+func toUDPAddr(b []byte) *net.UDPAddr {
+	addr := new(net.UDPAddr)
+	r := bytes.NewReader(b)
+
+	var ip [4]byte
+	if err := binary.Read(r, binary.BigEndian, &ip); err != nil {
+		return nil
+	}
+	addr.IP = ip[:]
+
+	var port uint16
+	if err := binary.Read(r, binary.BigEndian, &port); err != nil {
+		return nil
+	}
+	addr.Port = int(port)
+
+	return addr
+}
+
+func main() {
+	db, err := leveldb.OpenFile("leveldb", nil)
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	iter := db.NewIterator(nil, nil)
+	for iter.Next() {
+		key := iter.Key()
+		addr := toUDPAddr(key)
+		fmt.Println(addr)
+	}
+	iter.Release()
+}
